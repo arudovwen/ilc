ecab7947311bd0527ca311805e41bc94c439bb31
Arudovwen <successahon@gmail.com>
2020-07-21 20:52:19 +0100

    update

--- public/home_routes.js
+++ public/home_routes.js
@@ -2714,6 +2714,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: ["admin"],
   data: function data() {
--- resources/js/components/admin/student/assignLevel.vue
+++ resources/js/components/admin/student/assignLevel.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="body p-3">
     <div class="d-flex justify-content-around mb-5">
       <div class="w-25">
